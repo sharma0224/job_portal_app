fdf2e749a2495f6e592c738aa168d71fd0e1f9be
sharma0224 <sharmapulkit2304@gmail.com>
2025-01-15 00:50:37 +0530

    filter job & location

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useContext, useRef } from 'react'
 import { assets } from '../assets/assets'
+import { AppContext } from '../context/AppContext'
 
 const Hero = () => {
+
+    const {setSearchFilter, setIsSearched} = useContext(AppContext)
+
+    const titleRef = useRef(null)
+    const locationRef = useRef(null)
+
+    const onSearch = () => {
+        setSearchFilter({
+            title: titleRef.current.value,
+            location: locationRef.current.value
+        })
+        setIsSearched(true)
+        
+    }
+
   return (
     <div className='container 2xl:px-20 mx-auto my-10'>
         <div className='bg-gradient-to-r from-purple-900 to-purple-950 text-white py-16 text-center mx-2 rounded-xl'>
@@ -12,17 +28,35 @@ const Hero = () => {
                     <img className='h-4 sm:h-5' src={assets.search_icon} alt="" />
                     <input type="text" 
                     placeholder='Search for jobs'
-                    className='max-sm:text-xs p-2 rounded outline-none w-full'/>
+                    className='max-sm:text-xs p-2 rounded outline-none w-full'
+                    ref={titleRef}
+                    />
                 </div>
                 <div className='flex items-center'>
                     <img className='h-4 sm:h-5' src={assets.location_icon} alt="" />
                     <input type="text" 
                     placeholder='Location'
-                    className='max-sm:text-xs p-2 rounded outline-none w-full'/>
+                    className='max-sm:text-xs p-2 rounded outline-none w-full'
+                    ref={locationRef}
+                    />
                 </div>
-                <button className='bg-blue-600 px-6 py-2 rounded text-white m-1'>Search</button>
+                <button onClick={onSearch} className='bg-blue-600 px-6 py-2 rounded text-white m-1'>Search</button>
             </div>
         </div>
+
+        <div className='border border-gray-300 shadow-md mx-2 mt-5 p-6 rounded-md flex'>
+            <div className='flex justify-center gap-10 lg:gap-16 flex-wrap'>
+                <p className='font-medium'>Trusted by</p>
+                <img className='h-6' src={assets.microsoft_logo} alt="" />
+                <img className='h-6' src={assets.walmart_logo} alt="" />
+                <img className='h-6' src={assets.accenture_logo} alt="" />
+                <img className='h-6' src={assets.samsung_logo} alt="" />
+                <img className='h-6' src={assets.amazon_logo} alt="" />
+                <img className='h-6' src={assets.adobe_logo} alt="" />
+            </div>
+        </div>
+
+
     </div>
   )
 }
